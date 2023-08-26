adb764c7d93569715d8ce56c56d6cdabc0c838f3
devangdayal <devangdayal@gmail.com>
2023-08-26 12:52:30 +0530

    Added the Settings Module

--- chatman/app/components/sidebar/DesktopSidebar.tsx
+++ chatman/app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { User } from "@prisma/client";
 import DesktopItem from "./DesktopItem";
 import Avatar from "../Avatar";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps{
     currentUser : User;
@@ -22,43 +23,51 @@ const DesktopSidebar: React.FC<DesktopSidebarProps>= ({
 
     return (
 
-        <div className=" hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto
-         lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+        <>
+            <SettingsModal
+            currentUser= {currentUser}
+            isOpen={isOpen}
+            onClose={()=> setIsOpen(false)}
+            />
+            <div className=" hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto
+            lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
 
-            <nav className=" mt-4 flex flex-col justify-between">
+                <nav className=" mt-4 flex flex-col justify-between">
 
-                <ul role="list" className="flex flex-col items-center space-y-1">
+                    <ul role="list" className="flex flex-col items-center space-y-1">
 
-                    {
-                        routes.map((item) => (
-                            <DesktopItem
-                                key={item.label}
-                                href={item.href}
-                                label={item.label}
-                                icon={item.icon}
-                                active={item.active}
-                                onClick={item.onClick}
-                            />
+                        {
+                            routes.map((item) => (
+                                <DesktopItem
+                                    key={item.label}
+                                    href={item.href}
+                                    label={item.label}
+                                    icon={item.icon}
+                                    active={item.active}
+                                    onClick={item.onClick}
+                                />
 
-                        ))}
-                </ul>
-            </nav>
-            <nav
-            className=" mt-4 flex flex-col justify-between items-center">
-                <div onClick={() => setIsOpen(true)} 
-                className="cursor-pointor first-letter:hover:opacity-75 transition">
+                            ))
+                        }
+                    </ul>
+                </nav>
+                <nav
+                className=" mt-4 flex flex-col justify-between items-center">
+                    <div onClick={() => setIsOpen(true)} 
+                    className="cursor-pointor first-letter:hover:opacity-75 transition">
 
-                    <Avatar user = {currentUser} />
-
-
-
-
-                </div>
-
-            </nav>
+                        <Avatar user = {currentUser} />
 
 
-        </div>
+
+
+                    </div>
+
+                </nav>
+
+
+            </div>
+        </>
     );
 }
 
